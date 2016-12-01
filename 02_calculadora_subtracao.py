9400bb66539a21c526620df571f88ca0e38356f6
Jorge Klemm <jorge@meuspedidos.com.br>
2016-12-01 10:45:53 -0200

    Códigos convertidos para Python3

--- 02_calculadora_subtracao.py
+++ 02_calculadora_subtracao.py
@@ -1,13 +1,13 @@
-print 'Calculadora que faz Subtracao'
+print('Calculadora que faz Subtracao')
 
-numero_1 = raw_input('Informe o primeiro numero: ')
+numero_1 = input('Informe o primeiro numero: ')
 
-numero_2 = raw_input('Informe o segundo numero: ')
+numero_2 = input('Informe o segundo numero: ')
 
-numero_3 = raw_input('Informe o terceiro numero: ')
+numero_3 = input('Informe o terceiro numero: ')
 
 resultado = int(numero_1) - int(numero_2) - int(numero_3)
 
-print 'A subtracao desses numeros eh: {}'.format(resultado)
+print('A subtracao desses numeros eh: {}'.format(resultado))
 
-print 'Ou seja, {} - {} - {} = {}'.format(numero_1, numero_2, numero_3, resultado)
+print('Ou seja, {} - {} - {} = {}'.format(numero_1, numero_2, numero_3, resultado))
--- 03_calculadora_soma_e_subtracao.py
+++ 03_calculadora_soma_e_subtracao.py
@@ -1,30 +1,30 @@
-print 'Calculadora que faz 2 operacoes'
+print('Calculadora que faz 2 operacoes')
 
-operacao = raw_input('Informe a operacao desejada (1 - Soma, 2 - Subtracao): ')
+operacao = input('Informe a operacao desejada (1 - Soma, 2 - Subtracao): ')
 
 if operacao in ('1', '2'):
 
-    numero1 = raw_input('Informe o primeiro numero: ')
+    numero1 = input('Informe o primeiro numero: ')
 
-    numero2 = raw_input('Informe o segundo numero: ')
+    numero2 = input('Informe o segundo numero: ')
 
-    numero3 = raw_input('Informe o terceiro numero: ')
+    numero3 = input('Informe o terceiro numero: ')
 
     if operacao == '1':
 
         resultado = int(numero1) + int(numero2) + int(numero3)
 
-        print 'A soma desses numeros eh: {}'.format(resultado)
+        print('A soma desses numeros eh: {}'.format(resultado))
 
-        print 'Ou seja, {} + {} + {} = {}'.format(numero1, numero2, numero3, resultado)
+        print('Ou seja, {} + {} + {} = {}'.format(numero1, numero2, numero3, resultado))
 
     elif operacao == '2':
 
         resultado = int(numero1) - int(numero2) - int(numero3)
 
-        print 'A subtracao desses numeros eh: {}'.format(resultado)
+        print('A subtracao desses numeros eh: {}'.format(resultado))
 
-        print 'Ou seja, {} - {} - {} = {}'.format(numero1, numero2, numero3, resultado)
+        print('Ou seja, {} - {} - {} = {}'.format(numero1, numero2, numero3, resultado))
 
 else:
-    print 'Esta operacao nao existe, redigite outra..'
+    print('Esta operacao nao existe, redigite outra..')
