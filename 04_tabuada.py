ec07fb39c238b10c7fec64f64d24a21add430054
Jorge Klemm <jorge@meuspedidos.com.br>
2016-12-02 15:14:18 -0200

    Adicionados comentários no código

--- 04_tabuada.py
+++ 04_tabuada.py
@@ -2,10 +2,14 @@
 
 print('===== TABUADA =====')
 
+# obter o número que se deseja gerar a tabuada
 numero = input('Informe um número: ')
 
+# converter o número de string para int
+# se não converter, o cálculo matemático não funciona
 numero = int(numero)
 
+# gera a tabuada na tela, de 1 até 10
 print('{} x {} = {}'.format(numero, 1, numero * 1))
 print('{} x {} = {}'.format(numero, 2, numero * 2))
 print('{} x {} = {}'.format(numero, 3, numero * 3))
