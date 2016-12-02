8834c921beeac869aad69fcaac1b7b1b0ad83592
Jorge Klemm <jorge@meuspedidos.com.br>
2016-12-02 15:06:09 -0200

    Programa que faz tabuada

---
+++ 04_tabuada.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+print('===== TABUADA =====')
+
+numero = input('Informe um número: ')
+
+numero = int(numero)
+
+print('{} x {} = {}'.format(numero, 1, numero * 1))
+print('{} x {} = {}'.format(numero, 2, numero * 2))
+print('{} x {} = {}'.format(numero, 3, numero * 3))
+print('{} x {} = {}'.format(numero, 4, numero * 4))
+print('{} x {} = {}'.format(numero, 5, numero * 5))
+print('{} x {} = {}'.format(numero, 6, numero * 6))
+print('{} x {} = {}'.format(numero, 7, numero * 7))
+print('{} x {} = {}'.format(numero, 8, numero * 8))
+print('{} x {} = {}'.format(numero, 9, numero * 9))
+print('{} x {} = {}'.format(numero, 10, numero * 10))
