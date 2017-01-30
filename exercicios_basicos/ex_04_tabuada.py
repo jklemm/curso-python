8a4e922daa4ae85482ab4d20336f1f972fded1b3
Jorge Klemm <jorge@meuspedidos.com.br>
2017-01-30 15:34:54 -0200

    Convertida tabuada para utilizar comando FOR

--- exercicios_basicos/ex_04_tabuada.py
+++ exercicios_basicos/ex_04_tabuada.py
@@ -1,22 +1,12 @@
-# coding: utf-8
 
-print('===== TABUADA =====')
+def tabuada(numero):
+    for contador in range(1, 31):
+        print('{} x {} = {}'.format(numero, contador, numero * contador))
 
-# obter o número que se deseja gerar a tabuada
-numero = input('Informe um número: ')
 
-# converter o número de string para int
-# se não converter, o cálculo matemático não funciona
-numero = int(numero)
+if __name__ == '__main__':
+    print('===== TABUADA =====')
 
-# gera a tabuada na tela, de 1 até 10
-print('{} x {} = {}'.format(numero, 1, numero * 1))
-print('{} x {} = {}'.format(numero, 2, numero * 2))
-print('{} x {} = {}'.format(numero, 3, numero * 3))
-print('{} x {} = {}'.format(numero, 4, numero * 4))
-print('{} x {} = {}'.format(numero, 5, numero * 5))
-print('{} x {} = {}'.format(numero, 6, numero * 6))
-print('{} x {} = {}'.format(numero, 7, numero * 7))
-print('{} x {} = {}'.format(numero, 8, numero * 8))
-print('{} x {} = {}'.format(numero, 9, numero * 9))
-print('{} x {} = {}'.format(numero, 10, numero * 10))
+    numero_obtido = int(input('Informe um número: '))
+
+    tabuada(numero_obtido)
