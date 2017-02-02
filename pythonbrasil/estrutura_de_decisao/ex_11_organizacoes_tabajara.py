c9c533caa2e2da82496157444819c859933f1de3
Jorge Augusto Klemm <jorge@meuspedidos.com.br>
2017-02-02 10:07:03 -0200

    Refatoração do exercício 11 aumento salário

--- pythonbrasil/estrutura_de_decisao/ex_11_organizacoes_tabajara.py
+++ pythonbrasil/estrutura_de_decisao/ex_11_organizacoes_tabajara.py
@@ -5,24 +5,29 @@ def obter_salario_atual():
 
 def obter_porcentagem_de_aumento(salario):
     if salario <= 280:
-        porcentagem = 0.20
+        porcentagem = 20
     elif salario <= 700:
-        porcentagem = 0.15
+        porcentagem = 15
     elif salario <= 1500:
-        porcentagem = 0.10
+        porcentagem = 10
     else:
-        porcentagem = 0.05
+        porcentagem = 5
     return porcentagem
+
+
+def obter_valor_do_aumento(salario, porcentagem):
+    return salario * (porcentagem / 100)
+
 
 if __name__ == '__main__':
     print('Calculadora de salário ajustado')
 
     salario_atual = obter_salario_atual()
     porcentagem_de_aumento = obter_porcentagem_de_aumento(salario_atual)
-    valor_do_aumento = salario_atual * porcentagem_de_aumento
+    valor_do_aumento = obter_valor_do_aumento(salario_atual, porcentagem_de_aumento)
     novo_salario = salario_atual + valor_do_aumento
 
     print('Salário atual: {:.2f}'.format(salario_atual))
-    print('Porcentagem de aumento: {:.0f}%'.format(porcentagem_de_aumento * 100))
+    print('Porcentagem de aumento: {:.0f}%'.format(porcentagem_de_aumento))
     print('Valor do aumento: {:.2f}'.format(valor_do_aumento))
     print('Novo salário ajustado: {:.2f}'.format(novo_salario))
--- pythonbrasil/estrutura_de_decisao/tests/test_obter_porcentagem_de_aumento.py
+++ pythonbrasil/estrutura_de_decisao/tests/test_obter_porcentagem_de_aumento.py
@@ -7,14 +7,28 @@ class ObterPorcentagemDeAumentoTests(TestCase):
 
     def test_salario_igual_ou_abaixo_de_280(self):
         porcentagem = obter_porcentagem_de_aumento(200)
-        self.assertEqual(porcentagem, 0.20)
+        self.assertEqual(porcentagem, 20)
 
         porcentagem = obter_porcentagem_de_aumento(280)
-        self.assertEqual(porcentagem, 0.20)
+        self.assertEqual(porcentagem, 20)
 
     def test_salario_igual_ou_abaixo_de_700(self):
         porcentagem = obter_porcentagem_de_aumento(500)
-        self.assertEqual(porcentagem, 0.15)
+        self.assertEqual(porcentagem, 15)
 
         porcentagem = obter_porcentagem_de_aumento(700)
-        self.assertEqual(porcentagem, 0.15)
+        self.assertEqual(porcentagem, 15)
+
+    def test_salario_igual_ou_abaixo_de_1500(self):
+        porcentagem = obter_porcentagem_de_aumento(1000)
+        self.assertEqual(porcentagem, 10)
+
+        porcentagem = obter_porcentagem_de_aumento(1500)
+        self.assertEqual(porcentagem, 10)
+
+    def test_salario_maior_que_1500(self):
+        porcentagem = obter_porcentagem_de_aumento(1501)
+        self.assertEqual(porcentagem, 5)
+
+        porcentagem = obter_porcentagem_de_aumento(30000)
+        self.assertEqual(porcentagem, 5)
