7a44bce34c8e421415aa95500ac5d6a619bfda1c
Jorge Klemm <jorge@meuspedidos.com.br>
2018-04-10 10:54:26 -0300

    Alguns ajustes

--- exercicios_autorais/ex_04_merge_de_arquivos_json.py
+++ exercicios_autorais/ex_04_merge_de_arquivos_json.py
@@ -1,4 +1,4 @@
-import json
+import simplejson
 
 lista_de_urls = [
     {'application_id': 1, 'json': '{"/api/v1/produtos": {"metodos": ["GET", "POST"]}}'},
@@ -8,14 +8,18 @@ nova_lista_de_urls = {}
 
 for url in lista_de_urls:
     application_id = url['application_id']
-    rotas = json.loads(url['json'])
+    rotas = url['json']
     
     for rota, metodos in rotas.items():
+        rota = rota.replace('\\d+', 'd+')
         if application_id not in nova_lista_de_urls:
             nova_lista_de_urls[application_id] = {rota: metodos}
+        elif rota not in nova_lista_de_urls[application_id]:
+            nova_lista_de_urls[application_id][rota] = metodos
         else:
             for metodo in metodos['metodos']:
                 if metodo not in nova_lista_de_urls[application_id][rota]['metodos']:
                     nova_lista_de_urls[application_id][rota]['metodos'].append(metodo)
 
-print(nova_lista_de_urls)
+for application_id, rotas in nova_lista_de_urls.items():
+    print(application_id, simplejson.dumps(rotas).replace('d+', '\\\\d+'))
