457b89693effcf894b9e4976471cc42b34131618
Jorge Klemm <jorge@meuspedidos.com.br>
2018-05-04 13:53:33 -0300

    Altera o exercício calculadora soma para usar funções

--- exercicios_basicos/ex_01_calculadora_soma.py
+++ exercicios_basicos/ex_01_calculadora_soma.py
@@ -1,13 +1,27 @@
-print('Calculadora que faz Soma')
 
-numero_1 = input('Informe o primeiro numero: ')
+def obtem_numero_inteiro(texto):
+    return int(input(texto))
 
-numero_2 = input('Informe o segundo numero: ')
 
-numero_3 = input('Informe o terceiro numero: ')
+def efetua_soma(num_1=0, num_2=0, num_3=0):
+    return num_1 + num_2 + num_3
 
-resultado = int(numero_1) + int(numero_2) + int(numero_3)
 
-print('A soma desses numeros eh: {}'.format(resultado))
+def obtem_tres_numeros_inteiros():
+    numero_1 = obtem_numero_inteiro('Informe o primeiro numero: ')
+    numero_2 = obtem_numero_inteiro('Informe o segundo numero: ')
+    numero_3 = obtem_numero_inteiro('Informe o terceiro numero: ')
+    return numero_1, numero_2, numero_3
 
-print('Ou seja, {} + {} + {} = {}'.format(numero_1, numero_2, numero_3, resultado))
+
+def apresenta_resultados(numero_1, numero_2, numero_3, resultado):
+    print('A soma desses numeros eh: {}'.format(resultado))
+    print('Ou seja, {} + {} + {} = {}'.format(numero_1, numero_2, numero_3, resultado))
+
+
+if __name__ == '__main__':
+    print('Calculadora que faz Soma')
+
+    numero_1, numero_2, numero_3 = obtem_tres_numeros_inteiros()
+    resultado = efetua_soma(numero_1, numero_2, numero_3)
+    apresenta_resultados(numero_1, numero_2, numero_3, resultado)
