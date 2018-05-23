68a7fa8dbfe2d4bc52551d44e9e2765011c28cd0
Jorge Klemm <jorge@meuspedidos.com.br>
2018-05-23 13:42:46 -0300

    Exercício de Pizza

--- dojo_sabores_das_pizzas.py
+++ dojo_sabores_das_pizzas.py
@@ -22,29 +22,61 @@
 # todo: reduzir o problema utilizando apenas 2 usuários e menos sabores
 
 # todo: facilitar a solução utilizando 1 e 2 como False e 3 a 5 como True nas notas dos sabores
+from itertools import product
 
-usuarios = {
-    'tino': {
-        'Marguerita': 1,
-        'Quatro queijos': 5,
-        'Escarola': 1,
-        'Portuguesa': 4,
-        'Frango+Catupiry': 3,
-        'Napolitana': 2
-    },
-    'luca': {
-        'Marguerita': 5,
-        'Quatro queijos': 4,
-        'Escarola': 3,
-        'Portuguesa': 4,
-        'Frango+Catupiry': 3,
-        'Napolitana': 2
-    }
-}
+
+def substitui_notas_por_true_e_false(usuarios):
+    for usuario, sabores in usuarios.items():
+        for sabor, nota in sabores.items():
+            if nota >= 3:
+                usuarios[usuario][sabor] = True
+            else:
+                usuarios[usuario][sabor] = False
+    return usuarios
+
+
+def verifica_se_combinam(sabores_do_usuario_1, sabores_do_usuario_2):
+    quantidade_sabores_que_combinam = 0
+
+    for sabor1, nota in sabores_do_usuario_1.items():
+        nota_do_outro_usuario = sabores_do_usuario_2[sabor1]
+
+        if nota is True and nota_do_outro_usuario is True:
+            quantidade_sabores_que_combinam += 1
+
+    if quantidade_sabores_que_combinam >= 3:
+        return True
+    else:
+        return False
 
 
 def main():
+    usuarios = {
+        'Renato': {'Marguerita': 4, 'Quatro queijos': 5,'Escarola': 4,'Portuguesa': 5, 'Frango+Catupiry': 4, 'Napolitana': 3},
+        'Marcelo': {'Marguerita': 2, 'Quatro queijos': 2,'Escarola': 1,'Portuguesa': 3, 'Frango+Catupiry': 5, 'Napolitana': 2},
+        'Lenon': {'Marguerita': 4, 'Quatro queijos': 5,'Escarola': 2,'Portuguesa': 1, 'Frango+Catupiry': 1, 'Napolitana': 3},
+        'Renata': {'Marguerita': 4, 'Quatro queijos': 5,'Escarola': 1,'Portuguesa': 1, 'Frango+Catupiry': 3, 'Napolitana': 4},
+        'Washington': {'Marguerita': 1, 'Quatro queijos': 1,'Escarola': 2,'Portuguesa': 3, 'Frango+Catupiry': 4,'Napolitana': 3},
+        'Tino': {'Marguerita': 1, 'Quatro queijos': 5,'Escarola': 1,'Portuguesa': 4, 'Frango+Catupiry': 3, 'Napolitana': 2},
+        'Luca': {'Marguerita': 5, 'Quatro queijos': 4,'Escarola': 3,'Portuguesa': 4, 'Frango+Catupiry': 3, 'Napolitana': 2},
+    }
+
     print('Sabores das Pizzas')
+
+    usuarios = substitui_notas_por_true_e_false(usuarios)
+
+    usuario_desejado = input('Escreva o nome do usuário pra comparar sabores de pizza: ')
+
+    sabores_que_usuario_desejado_gosta = usuarios[usuario_desejado] 
+
+    for usuario, sabores in usuarios.items():
+    
+        if usuario == usuario_desejado:
+            continue
+    
+        curtem = verifica_se_combinam(sabores_que_usuario_desejado_gosta , sabores)
+    
+        print('{} e {}: {} curtem os mesmos sabores!'.format(usuario_desejado, usuario, '' if curtem else 'NÃO'))
         
 
 if __name__ == '__main__':
