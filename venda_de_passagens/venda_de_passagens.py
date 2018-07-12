3db30187ec9bd8ebd9f32fcc4a1f230b786b832d
Jorge Klemm <jorge@meuspedidos.com.br>
2018-07-12 09:17:34 -0300

    Início do programa venda de passagens

---
+++ venda_de_passagens/venda_de_passagens.py
@@ -0,0 +1,13 @@
+# * Criar um programa que gerencie a venda e ocupação de poltronas para uma viagem de ônibus.
+# Crianças (<5 anos) e idosos (>65 anos) pagam apenas 50% da passagem.
+# * Mantenha o programa em execução até que o operador decida não vender mais passagens.
+# * O programa não pode permitir que duas passagens para a mesma poltrona sejam vendidas.
+# * O programa deve cancelar a viagem se menos de 50% das passagens forem
+# vendidas quando o operador encerrar as vendas.
+# * Utilize 4 vetores para armazenar a ocupação para as poltronas - um vetor para cada fileira
+# de poltronas, sendo 2 para janela e 2 para o corredor.
+# * Ao final da execução, informe o faturamento total das vendas (em Reais), caso a viagem seja confirmada.
+# * Ao final da execução, informe se a viagem foi cancelada ou não.
+
+if __name__ == '__main__':
+    pass
