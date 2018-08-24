4413794f4733132577de98653cfde734d7f836ce
Jorge Klemm <jorge@meuspedidos.com.br>
2018-08-24 13:10:28 -0300

    Curso 2: exercício 2

---
+++ curso-2/exercicio_02.py
@@ -0,0 +1,9 @@
+
+# entrada de dados
+numero_obtido = input('Digite um número: ')
+
+# processamento
+mensagem = 'O número informado foi {}'.format(numero_obtido)
+
+# saída de dados
+print(mensagem)
