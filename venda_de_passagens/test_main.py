4fd4d99b16ef1d53e8c975b843dfea165e8ecb39
Jorge Klemm <jorge@meuspedidos.com.br>
2018-12-04 12:48:11 -0200

    Testa a função

---
+++ venda_de_passagens/test_main.py
@@ -0,0 +1,17 @@
+from venda_de_passagens.main import todas_poltronas_estao_vendidas
+
+
+def test_nao_sao_todas_as_poltronas_vendidas():
+    assert todas_poltronas_estao_vendidas(['', '', '']) is False
+    assert todas_poltronas_estao_vendidas(['X', '', '']) is False
+    assert todas_poltronas_estao_vendidas(['', 'X', '']) is False
+    assert todas_poltronas_estao_vendidas(['', '', 'X']) is False
+    assert todas_poltronas_estao_vendidas(['X', 'X', '']) is False
+    assert todas_poltronas_estao_vendidas(['', 'X', 'X']) is False
+    assert todas_poltronas_estao_vendidas(['X', '', 'X']) is False
+
+
+def test_todas_as_poltronas_vendidas():
+    assert todas_poltronas_estao_vendidas(['X']) is True
+    assert todas_poltronas_estao_vendidas(['X', 'X']) is True
+    assert todas_poltronas_estao_vendidas(['X', 'X', 'X']) is True
