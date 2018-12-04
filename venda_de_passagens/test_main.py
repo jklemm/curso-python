075f24dc7a36b3118d49ebb366236b2ec5bc5233
Jorge Klemm <jorge@meuspedidos.com.br>
2018-12-04 13:33:03 -0200

    Teste

--- venda_de_passagens/test_main.py
+++ venda_de_passagens/test_main.py
@@ -2,7 +2,7 @@ from venda_de_passagens.main import todas_poltronas_estao_vendidas
 
 
 def test_nao_sao_todas_as_poltronas_vendidas():
-    assert todas_poltronas_estao_vendidas(['', '', '']) is False
+    assert todas_poltronas_estao_vendidas(['', '', '']) is not True
     assert todas_poltronas_estao_vendidas(['X', '', '']) is False
     assert todas_poltronas_estao_vendidas(['', 'X', '']) is False
     assert todas_poltronas_estao_vendidas(['', '', 'X']) is False
